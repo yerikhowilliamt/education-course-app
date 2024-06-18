8f97ded3fe7ebd8fa2a647932994d2b558c76652
Yerikho William Tasilima <yerikhowilliamt@gmail.com>
2024-06-18 12:16:37 +0700

    second commit

--- src/components/Fragments/BannerProduct.jsx
+++ src/components/Fragments/BannerProduct.jsx
@@ -1,16 +1,10 @@
 const BannerProduct = ({ children, background }) => {
   return (
-      <div
-        className={`z-0 w-full h-[400px] flex flex-wrap justify-center items-center rounded-xl ${background} bg-cover bg-center after:relative  after:content-[''] after:bg-black after:bg-opacity-80 after:w-full after:h-[400px] after:top-0 after:left-0 after:rounded-xl`}
-      >
-        {/* <img
-        className="absolute w-full h-[400px] rounded-xl object-cover"
-        src={src}
-        alt=""
-      />
-      <div className="absolute inset-0 bg-black bg-opacity-70"></div> */}
+    <div
+      className={`z-0 w-full h-[400px] flex flex-wrap justify-center items-center rounded-xl ${background} bg-cover bg-center after:relative  after:content-[''] after:bg-black after:bg-opacity-80 after:w-full after:h-[400px] after:top-0 after:left-0 after:rounded-xl`}
+    >
       {children}
-      </div>
+    </div>
   );
 };
 
